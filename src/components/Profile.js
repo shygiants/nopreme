d4a43bdb0bfe1555a75584d408826d9e38315607
aromage <firerings02@gmail.com>
2020-02-18 21:40:20 +0900

    profile openchatlink regex

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,22 @@ import {Box, Text, Button, TextInput} from 'grommet';
 import {graphql, createFragmentContainer} from 'react-relay';
 import debounce from 'lodash.debounce';
 
+
+const nameStatusText = {
+    available:'사용할 수 있는 닉네임 입니다',
+    unavailable:'중복되거나 사용할 수 없는 닉네임 입니다'
+}
+
+const linkStatusText = {
+    available:'유효한 링크입니다',
+    unavailable:'유효하지 않은 링크입니다'
+}
+
+const statusColor = {
+    available:'status-ok',
+    unavailable:'status-error'
+}
+
 class Profile extends Component {    
     constructor(props) {
         super(props);
@@ -11,12 +27,14 @@ class Profile extends Component {
 
         this.state = {
             name: viewer.name,
-            openChatLink: viewer.openChatLink,        
+            openChatLink: viewer.openChatLink,
+            linkStatus: '',  
+            nameStatus: '',   
         };
 
         //마지막 input발생 후 500ms뒤에 실행합니다
-        this.debounceHandleNameChange = debounce(this.debounceHandleNameChange, 500);
-        this.debounceHandleOpenChatLinkChange = debounce(this.debounceHandleOpenChatLinkChange, 500);
+        this.debounceHandleNameChange = debounce(this.debounceHandleNameChange, 300);
+        this.debounceHandleOpenChatLinkChange = debounce(this.debounceHandleOpenChatLinkChange, 300);
     }
 
     handleNameChange(e) {
@@ -30,7 +48,17 @@ class Profile extends Component {
     debounceHandleNameChange(target){
         //디바운싱 로직 - 닉네임 중복 체크는 여기서 하면 됩니다
         console.log(target.value);
+        // if(true){
+        //     this.setState({
+        //         nameStatus: 'available'
+        //     })
+        // }else{
+        //     this.setState({
+        //         nameStatus: 'unavailable'
+        //     })
+        // }
     }
+
 
     handleOpenChatLinkChange(e){
         let newValue = e.target.value;
@@ -41,16 +69,24 @@ class Profile extends Component {
     }
 
     debounceHandleOpenChatLinkChange(target){
-        let newValue = target.value
-        console.log(newValue);
-        //디바운싱 로직 - Regex - 카카오톡 오픈채팅 링크 유효성 검사는 여기서
-        // let chatLinkPattern = new RegExp("");
-        // if(chatLinkPattern.test(newValue)){
-        //     //Regex 매칭 로직
-        // }    
+        const pattern = "^https?:\/\/(open\.kakao\.com)\/([a-zA-Z0-9]+)\/([a-zA-Z0-9]+)$"
+        let chatLinkPattern = new RegExp(pattern);
+        if(chatLinkPattern.test(target.value)){
+            this.setState({
+                linkStatus: 'available'
+            })
+        }else{
+            this.setState({
+                linkStatus: 'unavailable'
+            })
+        }
     }
 
-
+    /**
+     * 각 inputField에서 엔터 입력이 발생할 경우 해당 필드값
+     * 수정 요청을 보내기 위한 이벤트 핸들러입니다. 
+     * @param {event} e 
+     */
     handleKeyDown(e){
         if(e.key === 'Enter'){
             console.log(e.target.name +":"+e.target.value);
@@ -60,7 +96,8 @@ class Profile extends Component {
 
     render() {
         const {viewer} = this.props;
-        const {name, openChatLink} = this.state;
+        const {name, openChatLink, linkStatus, nameStatus} = this.state;
+
         return (
             <Box
                 pad='medium'
@@ -80,6 +117,12 @@ class Profile extends Component {
                         onChange={this.handleNameChange.bind(this)}
                         onKeyDown={this.handleKeyDown.bind(this)}
                     />
+                    <Text
+                        size='xsmall'
+                        color={statusColor[nameStatus]}
+                    >
+                        {nameStatusText[nameStatus]}
+                    </Text>
                 </Box>
                 <Box
                     direction='column'
@@ -94,6 +137,12 @@ class Profile extends Component {
                         onChange={this.handleOpenChatLinkChange.bind(this)}
                         onKeyDown={this.handleKeyDown.bind(this)}
                     />
+                    <Text 
+                        size='xsmall' 
+                        color={statusColor[linkStatus]}
+                    >
+                        {linkStatusText[linkStatus]}
+                    </Text>
                 </Box>
 
             </Box>
